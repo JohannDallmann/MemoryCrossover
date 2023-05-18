a1ba41dbeb910e013206262b8cd442e4d6f3e97d
KitNaIn <k.na-in@gmx.de>
2023-05-18 20:28:54 +0200

    added node in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
 import useLoadRandMCharacters from "./hooks/useLoadRandMCharacters";
 import CharacterGallery from "./characterGallery/CharacterGallery";
-import Axios from "axios";
-import {RandMCharacter} from "./model/RandMCharacter";
-import {BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
+import {Route, Link, Routes} from "react-router-dom";
 
 
 function App() {
-  const {characters} = useLoadRandMCharacters();
+    const {characters} = useLoadRandMCharacters();
 
 
-  return (
-    <div className="App">
-        <Routes>
-      <h1 className="Dashboard">Memory Crossover</h1>
-        <Link to="/rickandmortygallery">
-      <button className="button"> Rick and Morty Gallery</button>
-        </Link>
-            <Route path="/rickandmortygallery" element=
-                {<CharacterGallery characters={characters} />} />
-        </Routes>
+    return (
+        <div className="App">
+            <h1 className="Dashboard">Memory Crossover</h1>
+            <Link to="/rickandmortygallery">
+                <button className="button"> Rick and Morty Gallery</button>
+            </Link>
+            <Routes>
+                <Route path="/rickandmortygallery" element=
+                    {<CharacterGallery characters={characters}/>}/>
+            </Routes>
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
