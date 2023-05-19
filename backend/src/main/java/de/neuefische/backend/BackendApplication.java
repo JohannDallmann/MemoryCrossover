edc7083139faad9873caae4c8c3e169b1e6baee4
KitNaIn <k.na-in@gmx.de>
2023-05-19 12:39:21 +0200

    removed two unused imports in BackendApplication.java

--- backend/src/main/java/de/neuefische/backend/BackendApplication.java
+++ backend/src/main/java/de/neuefische/backend/BackendApplication.java
@@ -1,7 +1,5 @@
 package de.neuefische.backend;
 
-import de.neuefische.backend.service.RandMService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
