f201f87e02996a3fe29b28d20b17b7998c028b14
Oliver Hofrichter <oli.bremen@gmail.com>
2023-05-20 00:39:22 +0200

    - integration test class renamed

--- backend/src/test/java/de/neuefische/backend/controller/RandMControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/RandMControllerIntegrationTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class IntegrationTestsForRandMController {
+class RandMControllerIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
