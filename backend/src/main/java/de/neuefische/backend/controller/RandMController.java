a00aea0fd2a2879f57782bf053df32ce0cdaa194
Pavel Myshev <pavel.myshev@gmail.com>
2023-05-19 21:32:41 +0200

    deleted unused imports

--- backend/src/main/java/de/neuefische/backend/controller/RandMController.java
+++ backend/src/main/java/de/neuefische/backend/controller/RandMController.java
@@ -2,7 +2,6 @@ package de.neuefische.backend.controller;
 
 import lombok.RequiredArgsConstructor;
 import de.neuefische.backend.model.RandMCharacter;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
