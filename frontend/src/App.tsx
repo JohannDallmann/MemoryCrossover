39fd773df62697a69ec816f14b749dd09230f4c7
Oliver Hofrichter <oli.bremen@gmail.com>
2023-05-23 09:36:23 +0200

    implementing highscore list - added backend (model, repo, service, controller) - added frontend (link and route in home, custom hook, components for highscore list and highscore list entry with css, model for score, npm module moment)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,13 @@ import './App.css';
 import useLoadRandMCharacters from "./hooks/useLoadRandMCharacters";
 import CharacterGallery from "./characterGallery/CharacterGallery";
 import {Route, Link, Routes} from "react-router-dom";
+import useHighscore from "./hooks/useHighscore";
+import HighscoreList from "./highscoreList/HighscoreList";
 
 
 function App() {
     const {characters} = useLoadRandMCharacters();
+    const {scores} = useHighscore();
 
 
     return (
@@ -17,9 +20,14 @@ function App() {
             <Link to="/rickandmortygallery">
                 <button className="button"> Rick and Morty Gallery</button>
             </Link>
+            <Link to="/highscorelist">
+                <button className="button">Highscore List</button>
+            </Link>
             <Routes>
                 <Route path="/rickandmortygallery" element=
                     {<CharacterGallery characters={characters}/>}/>
+                <Route path="/highscorelist" element=
+                    {<HighscoreList scores={scores}/>}/>
             </Routes>
 
         </div>
---
+++ frontend/src/model/Score.ts
@@ -0,0 +1,5 @@
+export type Score = {
+    playerName:string,
+    score:number,
+    timestamp:Date
+}
