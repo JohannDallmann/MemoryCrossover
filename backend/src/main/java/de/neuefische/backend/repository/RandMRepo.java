89607ac00ee5859e513267c90f85abd39cf1176f
Oliver Hofrichter <oli.bremen@gmail.com>
2023-05-24 16:01:31 +0200

    removed unused imports

--- backend/src/main/java/de/neuefische/backend/repository/RandMRepo.java
+++ backend/src/main/java/de/neuefische/backend/repository/RandMRepo.java
@@ -1,15 +1,8 @@
 package de.neuefische.backend.repository;
 
 import de.neuefische.backend.model.RandMCharacter;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Repository
 public interface RandMRepo extends MongoRepository<RandMCharacter,String> {
