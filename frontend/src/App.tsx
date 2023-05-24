585a3093a0f57df9a2017921aa43cfe7e3a4f5e6
KitNaIn <k.na-in@gmx.de>
2023-05-24 13:05:09 +0200

    frontend pointer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,24 +12,18 @@ function App() {
     return (
 
         <div style={{ backgroundImage: "url('https://cdnb.artstation.com/p/assets/images/images/019/672/653/large/mohammed-gadi-rnm.jpg?1564526784')", backgroundAttachment: "fixed", backgroundSize: "cover", backgroundPosition: "center", minHeight: "100vh" }}>
+            <div className="header-container">
             <img className="neuefischeHeader" src={fische} alt="Gräte"/>
-            <span>
+
                  <Link to="home">
             <h1 className="Dashboard">Memory Crossover</h1>
         </Link>
-
-            </span>
-        <div>
-            <Link to="/rickandmortygallery">
-                <button className="button"> Rick and Morty Gallery</button>
-            </Link>
-            <Routes>
-                <Route path="/rickandmortygallery" element=
-                    {<CharacterGallery characters={characters}/>}/>
-            </Routes>
+            </div>
+        <div className="play-container">
             <img className="neuefischeLogo" src={neuefischehd} alt="Neue Fische Logo" />
             <h2 className="h2"> Java-Bo-23-1 </h2>
-                <p className="p1"> Die zeit ist gekommen dich zu beweisen. Bereite dich auf das Ultimative Memory vor. </p>
+                <p className="p1"> Die zeit ist gekommen dich zu beweisen.
+                    Bereite dich auf das Ultimative Memory vor. </p>
             <div className="available-button">
                 <div className="circle">
                     <div className="arrow"></div>
@@ -37,6 +31,13 @@ function App() {
                 <div className="text">Jetzt verfügbar</div>
             </div>
 
+            <Link to="/rickandmortygallery">
+                <button className="button"> Rick and Morty Gallery</button>
+            </Link>
+            <Routes>
+                <Route path="/rickandmortygallery" element=
+                    {<CharacterGallery characters={characters}/>}/>
+            </Routes>
 
 
         </div>
