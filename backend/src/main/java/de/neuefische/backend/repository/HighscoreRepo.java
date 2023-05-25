4a16c55a0feff349f4e87977b577f8891fa3167d
Oliver Hofrichter <oli.bremen@gmail.com>
2023-05-25 14:03:51 +0200

    using mongo repo

--- backend/src/main/java/de/neuefische/backend/repository/HighscoreRepo.java
+++ backend/src/main/java/de/neuefische/backend/repository/HighscoreRepo.java
@@ -1,41 +1,8 @@
 package de.neuefische.backend.repository;
 
 import de.neuefische.backend.model.Score;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+public interface HighscoreRepo extends MongoRepository<Score, String> {
 
-@Repository
-@AllArgsConstructor
-@Data
-public class HighscoreRepo {
-    private List<Score> highScoreList = new ArrayList<>();
-
-    public List<Score> addScore(Score score) {
-        highScoreList.add(score);
-        return highScoreList;
-    }
-
-    public List<Score> getScoresSortedAscendent() {
-        List<Score> sortedHighscores = new ArrayList<>(highScoreList);
-        Collections.sort(sortedHighscores, Comparator.comparingInt(Score::getScore));
-        return sortedHighscores;
-    }
-
-    public List<Score> getScoresSortedDescendent() {
-        List<Score> sortedHighscores = new ArrayList<>(highScoreList);
-        Collections.sort(sortedHighscores, Comparator.comparingInt(Score::getScore).reversed());
-        return sortedHighscores;
-    }
-
-    public List<Score> getScoresSortedByTimestamp() {
-        List<Score> sortedHighscores = new ArrayList<>(highScoreList);
-        Collections.sort(sortedHighscores, Comparator.comparing(Score::getTimestamp));
-        return sortedHighscores;
-    }
 }
