d83de93c07dc086643aa5203df6b2039c417c971
Oliver Hofrichter <oli.bremen@gmail.com>
2023-05-30 11:36:14 +0200

    added missing document annotation in user model

--- backend/src/main/java/de/neuefische/backend/model/MongoUser.java
+++ backend/src/main/java/de/neuefische/backend/model/MongoUser.java
@@ -3,10 +3,12 @@ package de.neuefische.backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Document("users")
 public class MongoUser {
     private String id;
     private String username;
