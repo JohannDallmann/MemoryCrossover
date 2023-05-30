15336bfd753f7449edb22a3a8aa3a1c404c19e3d
Oliver Hofrichter <oli.bremen@gmail.com>
2023-05-30 10:04:07 +0200

    added security filter chain

--- backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -2,11 +2,13 @@ package de.neuefische.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -25,5 +27,19 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .and().build();
     }
 }
