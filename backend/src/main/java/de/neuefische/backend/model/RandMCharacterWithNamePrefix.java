86840265bfe5c3a6c11e87b924d8224ff18fac5e
Pavel Myshev <pavel.myshev@gmail.com>
2023-05-30 13:28:38 +0200

    delete unused import

--- backend/src/main/java/de/neuefische/backend/model/RandMCharacterWithNamePrefix.java
+++ backend/src/main/java/de/neuefische/backend/model/RandMCharacterWithNamePrefix.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @EqualsAndHashCode(callSuper = true)
--- backend/src/main/java/de/neuefische/backend/repository/RandMRepo.java
+++ backend/src/main/java/de/neuefische/backend/repository/RandMRepo.java
@@ -3,7 +3,6 @@ package de.neuefische.backend.repository;
 import de.neuefische.backend.model.RandMCharacter;
 import de.neuefische.backend.model.GroupBySpecies;
 
-import de.neuefische.backend.model.RandMCharacterWithNamePrefixIntersection;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.Aggregation;
