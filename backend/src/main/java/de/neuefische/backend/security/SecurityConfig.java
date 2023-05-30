0e81a94638fc54d42c9b6562038f1dfdf1f8245b
Oliver Hofrichter <oli.bremen@gmail.com>
2023-05-30 09:54:49 +0200

    added SecurityConfig.java (hard coded user with InMemoryUserDetailsManager)

---
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package de.neuefische.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("max")
+                        .password("123")
+                        .roles("BASIC")
+                        .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
