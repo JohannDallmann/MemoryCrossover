0e08f5c267907a98d9465fff86d0d3b464a9135f
KitNaIn <k.na-in@gmx.de>
2023-06-01 11:32:32 +0200

    outsourced main elements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,26 @@
-import './App.css';
-import {Link, Route, Routes} from "react-router-dom";
-import fische from './images/fische.png'
-import DropdownMenu from "./dropdown/menu";
-import React, {useState} from "react";
-import useLoadRandMCharacters from "./hooks/useLoadRandMCharacters";
-import CharacterGallery from "./characterGallery/CharacterGallery";
-import Game from "./characterGallery/Game";
-import {CardCharacter} from "./model/CardCharacter";
-import Home from "./Home/Home";
-import useGetNRandomCards from "./hooks/useGetNRandomCards";
-
-import GalleryComponent from "./components/GalleryComponent";
-import HighscoreList from "./highscoreList/HighscoreList";
+import React, { useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import fische from './images/fische.png';
+import DropdownMenu from './dropdown/menu';
+import GalleryComponent from './components/GalleryComponent';
+import HighscoreList from './highscoreList/HighscoreList';
+import Game from './characterGallery/Game';
+import CharacterGallery from './characterGallery/CharacterGallery';
+import Home from './Home/Home';
+import useLoadRandMCharacters from './hooks/useLoadRandMCharacters';
+import useGetNRandomCards from './hooks/useGetNRandomCards';
 
 function App() {
-    const [character, setCharacter] = useState("");
-    const {characters} = useLoadRandMCharacters();
-    const {randomNCharacters} = useGetNRandomCards();
-
+    const [character, setCharacter] = useState('');
+    const { characters } = useLoadRandMCharacters();
+    const { randomNCharacters } = useGetNRandomCards();
 
     return (
-
         <div style={{ backgroundImage: "url('https://cdnb.artstation.com/p/assets/images/images/019/672/653/large/mohammed-gadi-rnm.jpg?1564526784')", backgroundAttachment: "fixed", backgroundSize: "cover", backgroundPosition: "center", minHeight: "100vh" }}>
             <div className="header-container">
                 <div className="placeholder">
                     <Link to="https://www.neuefische.de" target="_blank">
-                    <img className="neuefischeHeader" src={fische} alt="Gräte"/>
+                        <img className="neuefischeHeader" src={fische} alt="Gräte" />
                     </Link>
                     <Link to="/home" className="home">
                         <h1 className="Dashboard">Memory Crossover</h1>
@@ -33,11 +28,13 @@ function App() {
                 </div>
 
                 <div className="buttons-container">
-                    <DropdownMenu updateCharacter = {setCharacter}/>
+                    <DropdownMenu updateCharacter={setCharacter} />
                     <Link to="/home">
                         <button className="costume-button"> Home </button>
                     </Link>
-                    <button className="costume-button"> Play </button>
+                    <Link to="/game">
+                        <button className="costume-button"> Play </button>
+                    </Link>
                     <Link to="/highscorelist">
                         <button className="costume-button">Highscore </button>
                     </Link>
@@ -47,26 +44,27 @@ function App() {
                 <div className="play-container">
                     <h2 className="h2"> Java-Bo-23-1 </h2>
                     <p className="p1"> Die zeit ist gekommen dich zu beweisen.
-                            <br/>
-                            Bereite dich auf das Ultimative Memory vor. </p>
+                        <br />
+                        Bereite dich auf das Ultimative Memory vor. </p>
                     <div className="available-button">
                         <div className="circle">
                             <div className="arrow"></div>
                         </div>
-                            <div className="text">Jetzt verfügbar</div>
+                        <div className="text">Jetzt verfügbar</div>
                     </div>
                 </div>
                 <div className="character-gallery-wrapper">
-                    <GalleryComponent character={character}/>
+                    <GalleryComponent character={character} />
                     <Routes>
-                        <Route path="/highscorelist" element=
-                            {<HighscoreList/>}/>
+                        <Route path="/home" element={<Home character={character} />} />
+                        <Route path="/game" element={<Game randomNCharacters={randomNCharacters} />} />
+                        <Route path="/highscorelist" element={<HighscoreList />} />
+                        <Route path="/rickandmortygallery" element={<CharacterGallery characters={characters} />} />
                     </Routes>
                 </div>
             </div>
-
-
         </div>
     );
 }
+
 export default App;
--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -1,30 +1,80 @@
-import React from 'react';
-import GalleryComponent from "../components/GalleryComponent";
+import React, { useState } from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+import GalleryComponent from '../components/GalleryComponent';
+import Game from '../characterGallery/Game';
+import CharacterGallery from '../characterGallery/CharacterGallery';
+import HighscoreList from '../highscoreList/HighscoreList';
+import fische from '../images/fische.png';
+import DropdownMenu from '../dropdown/menu';
+import './Home.css';
+import gameCard from "../characterCard/GameCard";
+import useLoadRandMCharacters from "../hooks/useLoadRandMCharacters";
+import useGetNRandomCards from "../hooks/useGetNRandomCards";
 
-type Props ={
-    character: string
-}
 
-function Home(props:Props) {
+type Props = {
+    character: string;
+};
+
+function Home(props: Props) {
+    const [character, setCharacter] = useState('');
+    const {characters} = useLoadRandMCharacters();
+    const { randomNCharacters } = useGetNRandomCards();
+
     return (
         <div>
-            <div className="Frankenstein">
-                <div className="play-container">
-                    <h2 className="h2"> Java-Bo-23-1 </h2>
-                    <p className="p1"> Die zeit ist gekommen dich zu beweisen.
-                        <br/>
-                        Bereite dich auf das Ultimative Memory vor. </p>
-                    <div className="available-button">
-                        <div className="circle">
-                            <div className="arrow"></div>
+            <header>
+                <div className="Frankenstein">
+                    <div className="play-container">
+                        <h2 className="h2"> Java-Bo-23-1 </h2>
+                        <p className="p1">
+                            Die Zeit ist gekommen, dich zu beweisen.
+                            <br />
+                            Bereite dich auf das ultimative Memory vor.
+                        </p>
+                        <div className="available-button">
+                            <div className="circle">
+                                <div className="arrow"></div>
+                            </div>
+                            <div className="text">Jetzt verfügbar</div>
                         </div>
-                        <div className="text">Jetzt verfügbar</div>
+                    </div>
+                    <div className="character-gallery-wrapper">
+                        <GalleryComponent character={props.character} />
                     </div>
                 </div>
-                <div className="character-gallery-wrapper">
-                    <GalleryComponent character={props.character}/>
+
+                <div className="header-container">
+                    <div className="placeholder">
+                        <a href="https://www.neuefische.de" target="_blank" rel="noopener noreferrer">
+                            <img className="neuefischeHeader" src={fische} alt="Gräte" />
+                        </a>
+                        <Link to="/home" className="home">
+                            <h1 className="Dashboard">Memory Crossover</h1>
+                        </Link>
+                    </div>
+
+                    <div className="buttons-container">
+                        <DropdownMenu updateCharacter={setCharacter} />
+                        <Link to="/home">
+                            <button className="costume-button">Home</button>
+                        </Link>
+                        <Link to="/play">
+                            <button className="costume-button">Play</button>
+                        </Link>
+                        <Link to="/highscorelist">
+                            <button className="costume-button">Highscore</button>
+                        </Link>
+                    </div>
                 </div>
-            </div>
+            </header>
+
+            <Routes>
+                <Route path="/home" element={<Home character={character} />} />
+                <Route path="/play" element={<Game randomNCharacters={randomNCharacters} />} />
+                <Route path="/highscorelist" element={<HighscoreList />} />
+                <Route path="/rickandmortygallery" element={<CharacterGallery  characters={characters}/>} />
+            </Routes>
         </div>
     );
 }
