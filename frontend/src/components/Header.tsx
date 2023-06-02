e32addb91375d79a5707395e5c25eb84b57ef123
KitNaIn <k.na-in@gmx.de>
2023-06-02 16:19:11 +0200

    a

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import fische from '../images/fische.png';
 import DropdownMenu from '../dropdown/menu';
