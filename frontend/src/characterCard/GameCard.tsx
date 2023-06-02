8ce323cac68b22c757f6d8a557045c3979890e1c
KitNaIn <k.na-in@gmx.de>
2023-06-02 16:13:02 +0200

    supressing sonarcloud for one line s116 rndmcharacterwithnameprefix

--- frontend/src/characterCard/GameCard.tsx
+++ frontend/src/characterCard/GameCard.tsx
@@ -1,41 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './CharacterCard.css';
-import {CardCharacter} from "../model/CardCharacter";
+import { CardCharacter } from "../model/CardCharacter";
 
+type Props = {
+    character: CardCharacter;
+    increaseCounter: () => void;
+    putCardsInArrayToCompare: (currentCard: CardCharacter) => void;
+    counter: number;
+    selectedCardImage?: string | null;
+};
 
-type Props={
-    character:CardCharacter,
-    increaseCounter: () => void,
-    putCardsInArrayToCompare: (currentCard:CardCharacter) => void,
-    counter:number
-}
-
-function GameCard(props:Props) {
-
-    const {character} = props;
-
+function GameCard(props: Props) {
+    const { character, increaseCounter, putCardsInArrayToCompare, counter, selectedCardImage } = props;
 
     function showCard() {
         character.hidden = false;
-        props.increaseCounter();
-        props.putCardsInArrayToCompare(character);
+        increaseCounter();
+        putCardsInArrayToCompare(character);
     }
+
+    const backgroundImage = selectedCardImage || 'images/defaultcardback.jpeg';
 
     return (
         <div>
-            {
-                character.hidden
-                ? <button className="card-back" onClick={showCard}></button>
-                : <div className="character-card">
-                    <img src={character.image}  alt={character.name}/>
+            {character.hidden ? (
+                <div
+                    onClick={showCard}
+                    className="card-back"
+                    style={{ backgroundImage: `url(${backgroundImage})` }}
+                ></div>
+            ) : (
+                <div className="character-card">
+                    <img src={character.image} alt={character.name} />
                     <h3>{props.character.name}</h3>
                     <h4>{props.character.comparison}</h4>
                 </div>
-            }
-
+            )}
         </div>
-
     );
 }
 
 export default GameCard;
+
+
+
+
+
+
--- frontend/src/characterGallery/Game.tsx
+++ frontend/src/characterGallery/Game.tsx
@@ -1,27 +1,35 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import GameCard from "../characterCard/GameCard";
-import {CardCharacter} from "../model/CardCharacter";
+import { CardCharacter } from "../model/CardCharacter";
 import './Game.css';
+import defaultcardback from '../images/defaultcardback.jpeg'
+import cardback1 from '../images/cardback1.gif';
+import cardback3 from '../images/cardback3.gif';
+import cardback6 from '../images/cardback6.gif';
+
 
 type Props = {
-    cards:CardCharacter[];
-    counter:number;
+    cards: CardCharacter[];
+    counter: number;
     setCounter: (counter: number) => void;
-}
+};
 
-function Game(props:Props) {
-    const [selectedCards, setSelectedCards] = useState<CardCharacter[]>([])
+function Game(props: Props) {
+    const [selectedCards, setSelectedCards] = useState<CardCharacter[]>([]);
+    const [selectedCard, setSelectedCard] = useState<number | null>(null);
+    const [selectedCardImage, setSelectedCardImage] = useState<string | null>(defaultcardback);
 
-    function increaseCounter(){
+    function increaseCounter() {
         props.setCounter(props.counter + 1);
     }
 
-    function putCardsInArrayToCompare(currentCard:CardCharacter){
+    function putCardsInArrayToCompare(currentCard: CardCharacter) {
         setSelectedCards([...selectedCards, currentCard]);
     }
 
     useEffect(() => {
         if (selectedCards.length === 2) {
+            // eslint-disable-next-line react-hooks/exhaustive-deps
             compareCards();
             setSelectedCards([]);
         }
@@ -31,37 +39,64 @@ function Game(props:Props) {
         const firstCard = selectedCards[0];
         const secondCard = selectedCards[1];
 
-        if (firstCard.comparison === secondCard.comparison){
+        if (firstCard.comparison === secondCard.comparison) {
             firstCard.image = "https://t4.ftcdn.net/jpg/01/14/37/81/360_F_114378130_Zn6r0Vi0io6jTaKNEwW1B0F7dNyLAlva.jpg";
-            secondCard.image = "https://t4.ftcdn.net/jpg/01/14/37/81/360_F_114378130_Zn6r0Vi0io6jTaKNEwW1B0F7dNyLAlva.jpg"
+            secondCard.image = "https://t4.ftcdn.net/jpg/01/14/37/81/360_F_114378130_Zn6r0Vi0io6jTaKNEwW1B0F7dNyLAlva.jpg";
         } else {
-            setTimeout(() => hideCards(firstCard,secondCard),0);
+            setTimeout(() => hideCards(firstCard, secondCard), 0);
         }
     }
 
-    function hideCards(firstCard:CardCharacter,secondCard:CardCharacter){
+    function hideCards(firstCard: CardCharacter, secondCard: CardCharacter) {
         firstCard.hidden = true;
         secondCard.hidden = true;
     }
 
+    const cards = useMemo(() => [
+        { id: 1, image: cardback1 },
+        { id: 3, image: cardback3 },
+        { id: 6, image: cardback6 },
+        { id: 7, image: defaultcardback }
+    ], []);
+
+    useEffect(() => {
+        if (selectedCard !== null) {
+            const selectedCardObject = cards.find((card) => card.id === selectedCard);
+            if (selectedCardObject) {
+                setSelectedCardImage(selectedCardObject.image);
+            }
+        }
+    }, [selectedCard, cards]);
+
     return (
-        <div >
-            <div className={"status-bar"}>
-                <div className={"turns-counter"}>
-                    {"Current Counter: " + props.counter}
-                </div>
-                <div className={"timer"}>Time left: time-test</div>
+        <div>
+            <div className="status-bar">
+                <div className="turns-counter">Current Counter: {props.counter}</div>
+                {cards.map((card) => (
+                    <div
+                        key={card.id}
+                        className={`mini-card ${selectedCard === card.id ? "selected" : ""}`}
+                        onClick={() => setSelectedCard(card.id)}
+                    >
+                        <img src={card.image} alt={`Card ${card.id}`} />
+                    </div>
+                ))}
+                <div className="timer">Time left: Test-time</div>
             </div>
 
             <div className="card-container">
-                {props.cards.map((currentCharacter:CardCharacter)=>{
-                    return <GameCard key={currentCharacter.id}
-                                     character={currentCharacter}
-                                     putCardsInArrayToCompare={putCardsInArrayToCompare}
-                                     increaseCounter={increaseCounter}
-                                     counter ={props.counter}></GameCard>
-                })
-                }
+                {props.cards.map((currentCharacter: CardCharacter) => {
+                    return (
+                        <GameCard
+                            key={currentCharacter.id}
+                            character={currentCharacter}
+                            putCardsInArrayToCompare={putCardsInArrayToCompare}
+                            increaseCounter={increaseCounter}
+                            counter={props.counter}
+                            selectedCardImage={selectedCardImage}
+                        />
+                    );
+                })}
             </div>
         </div>
     );
