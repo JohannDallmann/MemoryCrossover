fe82b2e700043f2d1bed103ed2ee2a4e4308d9d3
Oliver Hofrichter <oli.bremen@gmail.com>
2023-06-05 10:30:40 +0200

    removed commented-out lines of code

--- backend/src/main/java/de/neuefische/backend/dto/CardDTO.java
+++ backend/src/main/java/de/neuefische/backend/dto/CardDTO.java
@@ -12,7 +12,5 @@ public class CardDTO {
     private int id;
     private String name;
     private String image;
-    //private String species;
-    //private boolean hidden;
     private String comparison;
 }
