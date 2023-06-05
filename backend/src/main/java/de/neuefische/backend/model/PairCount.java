6a6b381dfade8469927bd2be08259f287151d86f
Oliver Hofrichter <oli.bremen@gmail.com>
2023-06-05 11:36:10 +0200

    fixed sonarcloud security hotspot

--- backend/src/main/java/de/neuefische/backend/model/PairCount.java
+++ backend/src/main/java/de/neuefische/backend/model/PairCount.java
@@ -3,6 +3,8 @@ package de.neuefische.backend.model;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
+import java.security.SecureRandom;
+
 @Component
 @Data
 public class PairCount {
@@ -16,8 +18,9 @@ public class PairCount {
             numberOfCardsRickAndMorty = numberOfCardsTotal / 2;
             numberOfPairsGameOfThrones = numberOfPairs / 2;
         } else {
+            SecureRandom random = new SecureRandom();
             int remainder = 1;
-            if (Math.random() < 0.5) {
+            if (random.nextBoolean()) {
                 numberOfCardsRickAndMorty = 2 * (numberOfPairs / 2) + 2 * remainder;
                 numberOfPairsGameOfThrones = numberOfPairs / 2;
             } else {
